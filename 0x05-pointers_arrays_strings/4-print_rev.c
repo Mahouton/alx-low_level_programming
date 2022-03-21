98af473841980c0be7a62aaa673eb8980f93680f
Paul KPADONOU <pmkpaul87@gmail.com>
2022-03-21 20:19:23 +0100

    More pointers C programs

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,14 +2,14 @@
 
 /**
  * print_rev - Prints a string in reverse, followed by a new line.
- * @s: The pointer 
+ * @s: The pointer
  *
  */
 void print_rev(char *s)
 {
 	int length = 0, i;
 
-	while (*s++)
+	while (s[i])
 		length++;
 	for (i = length - 1; i >= 0; i--)
 		_putchar(s[i]);
