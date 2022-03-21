7501af98a326f91a52adc9d111585ad370da1e7b
Paul KPADONOU <pmkpaul87@gmail.com>
2022-03-21 21:06:02 +0100

    C program

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,8 +14,8 @@ void rev_string(char *s)
 		length++;
 	for (i = length - 1; i >= length / 2; i--)
 	{
-		tmp = s[i];
-		s[i] = s[length - i - 1];
-		s[length - i - 1] = tmp;
+		tmp = s[length - i - 1];
+		s[length - i - 1] = s[i];
+		s[i] = tmp;
 	}
 }
