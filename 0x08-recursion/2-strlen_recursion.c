a3fbd48498ef2cfccf3db1b8099321e33510ae57
Paul KPADONOU <pmkpaul87@gmail.com>
2022-03-30 22:15:58 +0100

    Recursion program

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,6 +4,7 @@
  * _strlen_recursion - Return the length of a string
  * @s: the string whose length will be returned
  *
+ * Return: the length of the string
  */
 int _strlen_recursion(char *s)
 {
